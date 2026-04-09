28c6916f738c9b9609183e055c8662fc002c461d
Rodge118 <l.s.rodgers9@outlook.com>
2026-04-09 20:14:58 +0000

    Removed home button in favor of 'logo' thing, tried and failed to add favicon, made it more mobile-friendly

--- header.js
+++ header.js
@@ -5,6 +5,7 @@ document.getElementById("header").innerHTML = `<!DOCTYPE html>
     <meta charset="UTF-8">
     <title>Rodge118's Site</title>
     <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
   </head>
 
   <nav id="navbar">
